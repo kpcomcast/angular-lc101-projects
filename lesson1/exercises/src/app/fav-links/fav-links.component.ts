6472687ab6b1fdd55ea9ae181703fd3afab4a0cf
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-06-15 14:37:59 -0600

    updated website with css styling

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+
+  favLinks = ["https://www.google.com/", "https://en.wikipedia.org/wiki/Main_Page"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
