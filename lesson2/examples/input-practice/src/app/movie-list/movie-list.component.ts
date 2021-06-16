2ff88caceef8b9f50a32890ee6dca014b7b583cf
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-06-16 12:06:50 -0600

    lesson2 examples completed

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   newType:string = "empty";
 
    constructor() { }
 
    ngOnInit() {
    }
+
+  //  addMovie(movie: string): void {
+  //    if (movie.length > 0) {
+  //      if (!this.movies.includes(movie)) {
+  //        this.movies.push(movie);
+  //        this.newType = "empty"
+  //      } else {
+  //        this.newType = movie;
+  //      }
+  //    } else {
+  //      this.newType = "";
+  //    }
+  //  }
+
+  addMovie(movie:string): string {
+    let errorMsg = "";
+    if (movie === "") {
+      errorMsg = "Please enter title.";
+    } else if (this.movies.includes(movie)) {
+      errorMsg = `${movie} is already listed.`;
+    } else {
+      this.movies.push(movie);
+    }
+    return errorMsg;
+  }
 }
