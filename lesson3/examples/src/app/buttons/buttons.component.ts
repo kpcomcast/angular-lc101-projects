8c4cc38100227800bb20ac5160de16eebe722c54
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-06-17 10:01:55 -0600

    angular3 examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: 'buttons',
+  templateUrl: './buttons.component.html',
+  styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons"
+  inactive: boolean = false;
+  goldOn: boolean = false;
+  silverOn: boolean = false;
+  copperOn: boolean = false;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
+
+  reset(): void {
+    let buttons: boolean[] = [this.goldOn, this.silverOn, this.copperOn];
+    for (let i of buttons) {
+      if (i) {
+        buttons[buttons.indexOf(i)] = !i
+      }
+    }
+    this.goldOn = buttons[0];
+    this.silverOn = buttons[1];
+    this.copperOn = buttons[2];
+  }
 
 }
